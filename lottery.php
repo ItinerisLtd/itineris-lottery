4d4ea325f8d53b040247e305240c609d7adb21f4
Tang Rufus <tangrufus@gmail.com>
2018-05-11 04:58:40 +0800

    :gun: You've got to ask yourself one question: Do I feel lucky? Well, do ya punk?
    https://youtu.be/8Xjr2hnOHiM?t=2m

---
+++ lottery.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name:     Lottery
+ * Plugin URI:      https://www.itineris.co.uk/
+ * Description:     Custom post type for lottery results
+ * Version:         0.1.0
+ * Author:          Itineris Limited
+ * Author URI:      https://www.itineris.co.uk/
+ * Text Domain:     lottery
+ */
+
+declare(strict_types=1);
+
+namespace Itineris\SagePay;
+
+// If this file is called directly, abort.
+if (! defined('WPINC')) {
+    die;
+}
+
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
+/**
+ * Begins execution of the plugin.
+ *
+ * @return void
+ */
+function run(): void
+{
+}
+
+run();
